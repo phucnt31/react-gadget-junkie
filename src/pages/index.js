d556790f62e71001689c5a2b7bcf93979e110b66
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-11 19:25:09 +0700

    Create pages and export them

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./Home";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
+import About from "./About";
+import Cart from "./Cart";
+import Error from "./Error";
+import Checkout from "./Checkout";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  Home,
+  SingleProduct,
+  Cart,
+  About,
+  Error,
+  Checkout,
+  Products,
+  PrivateRoute,
+};
