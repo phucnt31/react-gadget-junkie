3b96d9c70001207cac1c2f9b32d6d65e6fff2935
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-11 19:30:08 +0700

    Setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  Home,
+  About,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from "./pages";
+import { Navbar, Footer, Sidebar } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
