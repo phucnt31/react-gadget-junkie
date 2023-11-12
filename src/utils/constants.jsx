1a4b72b8103f5636728d010ced4aa0257bad8145
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-12 15:16:10 +0700

    Create dummy data to render in Services.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -17,3 +17,24 @@ export const links = [
     url: "/products",
   },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: <GiCompass />,
+    title: "mission",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 2,
+    icon: <GiDiamondHard />,
+    title: "vision",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 3,
+    icon: <GiStabbedNote />,
+    title: "history",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+];
