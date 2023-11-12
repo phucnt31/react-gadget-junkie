a45662c38ecde63892c149b4d0154740aff57770
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-12 14:33:18 +0700

    Setup reducer and actions

---
+++ src/reducers/products_reducer.js
@@ -0,0 +1,8 @@
+import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+
+const products_reducer = (state, action) => {
+  return state;
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default products_reducer;
