09d7e4cdf5de87133722a967687d3b915659240c
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-12 14:52:12 +0700

    Create PageHero component and export it

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
 import CartButtons from "./CartButtons";
+import PageHero from "./PageHero";
 
-export { Navbar, Footer, Sidebar, CartButtons };
+export { Navbar, Footer, Sidebar, CartButtons, PageHero };
