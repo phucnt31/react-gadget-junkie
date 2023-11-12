8b1dc7ca5f8007ac5de3550f24e2d1617827dcef
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-12 15:03:32 +0700

    Create new components and export them

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,19 @@ import Footer from "./Footer";
 import Sidebar from "./Sidebar";
 import CartButtons from "./CartButtons";
 import PageHero from "./PageHero";
+import Contact from "./Contact";
+import Hero from "./Hero";
+import FeaturedProducts from "./FeaturedProducts";
+import Services from "./Services";
 
-export { Navbar, Footer, Sidebar, CartButtons, PageHero };
+export {
+  Navbar,
+  Footer,
+  Sidebar,
+  CartButtons,
+  PageHero,
+  Contact,
+  Hero,
+  FeaturedProducts,
+  Services,
+};
