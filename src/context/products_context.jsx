349813f4c5fdd2f59c2864e245c327c82523887b
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-12 15:42:41 +0700

    Add new actions and dispatch to reducer

--- src/context/products_context.jsx
+++ src/context/products_context.jsx
@@ -1,10 +1,21 @@
 import axios from "axios";
 import React, { createContext, useContext, useEffect, useReducer } from "react";
 import reducer from "../reducers/products_reducer";
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_ERROR,
+  GET_PRODUCTS_SUCCESS,
+} from "../actions";
+import { products_url as url } from "../utils/constants";
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = createContext();
@@ -18,6 +29,21 @@ export const ProductsProvider = ({ children }) => {
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
+
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const resp = await axios.get(url);
+      const products = resp.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
+  };
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
 
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
