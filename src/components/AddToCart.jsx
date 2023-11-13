c235760afe8a83bd7239c82f4539191cc65745e4
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-13 20:58:04 +0700

    Customize AmountButtons.jsx and finished decrease, increase function

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -2,10 +2,27 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { FaCheck } from "react-icons/fa";
+import AmountButtons from "./AmountButtons";
 
 const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) tempAmount = stock;
+      return tempAmount;
+    });
+  };
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) tempAmount = 1;
+      return tempAmount;
+    });
+  };
 
   return (
     <Wrapper>
@@ -29,7 +46,16 @@ const AddToCart = ({ product }) => {
           })}
         </div>
       </div>
-      <div className="btn-container"></div>
+      <div className="btn-container">
+        <AmountButtons
+          increase={increase}
+          decrease={decrease}
+          amount={amount}
+        />
+        <Link to="/cart" className="btn">
+          add to cart
+        </Link>
+      </div>
     </Wrapper>
   );
 };
