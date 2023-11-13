72e9e89d01bb1aca3c69f366e0ec025bbe78f58a
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-13 21:10:24 +0700

    Load products for filter context

--- src/context/filter_context.jsx
+++ src/context/filter_context.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext, useReducer, createContext } from "react";
 import reducer from "../reducers/filter_reducer";
 import { useProductsContext } from "./products_context";
+import { LOAD_PRODUCTS } from "../actions";
 
 const initialState = {
   filtered_products: [],
@@ -11,6 +12,11 @@ const FilterContext = createContext();
 
 export const FilterProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { products } = useProductsContext();
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
 
   return (
     <FilterContext.Provider value={{ ...state }}>
