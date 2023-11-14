2ccc25420ecb5deaa3296e1b118d09e6b40dd06e
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-14 18:47:01 +0700

    Persist cart in local storage

--- src/context/cart_context.jsx
+++ src/context/cart_context.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useContext, useReducer, createContext } from "react";
 import reducer from "../reducers/cart_reducer";
 import { ADD_TO_CART } from "../actions";
 
+const getCartFromLocalStorage = () =>
+  JSON.parse(localStorage.getItem("cart")) || [];
+
 const initialState = {
-  cart: [],
+  cart: getCartFromLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -18,9 +21,21 @@ export const CartProvider = ({ children }) => {
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
+  // remove item
+  const removeItem = (id) => {};
+  // toggle amount
+  const toggleAmount = (id, value) => {};
+  // clear cart
+  const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, clearCart, toggleAmount }}
+    >
       {children}
     </CartContext.Provider>
   );
