740d7c848af4d95eb805766c8acb0075d255d9b3
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-15 20:20:12 +0700

    Setup Auth0

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,22 @@ import "./index.css";
 import { ProductsProvider } from "./context/products_context.jsx";
 import { FilterProvider } from "./context/filter_context.jsx";
 import { CartProvider } from "./context/cart_context.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain={import.meta.env.VITE_AUTH_DOMAIN}
+    clientId={import.meta.env.VITE_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>
 );
