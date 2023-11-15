4363a28fb5b0ae6bf7b8ccd4051fef1c6c4b7abc
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-15 20:33:46 +0700

    Setup login/logout button

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useProductsContext } from "../context/products_context";
 import { useCartContext } from "../context/cart_context";
+import { useUserContext } from "../context/user_context";
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
   const { total_items } = useCartContext();
+  const { loginWithRedirect, myUser, logout } = useUserContext();
 
   return (
     <Wrapper className="cart-btn-wrapper">
@@ -18,8 +20,15 @@ const CartButtons = () => {
           <span className="cart-value">{total_items}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn">
+      <button type="button" className="auth-btn" onClick={loginWithRedirect}>
         Login <FaUserPlus />
+      </button>
+      <button
+        type="button"
+        className="auth-btn"
+        onClick={() => logout({ returnTo: window.location.origin })}
+      >
+        Logout <FaUserMinus />
       </button>
     </Wrapper>
   );
