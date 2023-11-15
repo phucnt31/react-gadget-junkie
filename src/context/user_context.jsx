fe74588d95fa794f3b8af8137b4c039532f0c660
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-15 20:25:47 +0700

    Create user context

---
+++ src/context/user_context.jsx
@@ -0,0 +1,13 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const UserContext = createContext();
+export const UserProvider = ({ children }) => {
+  return (
+    <UserContext.Provider value="user context">{children}</UserContext.Provider>
+  );
+};
+// make sure use
+export const useUserContext = () => {
+  return useContext(UserContext);
+};
