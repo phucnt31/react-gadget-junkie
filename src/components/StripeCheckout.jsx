5a3af0434c1e49196f3637901711532774a9a5a3
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-16 21:34:13 +0700

    Setup StripeCheckout.jsx

--- src/components/StripeCheckout.jsx
+++ src/components/StripeCheckout.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  CardElement,
+  useStripe,
+  Elements,
+  useElements,
+} from "@stripe/react-stripe-js";
+import axios from "axios";
+import { useCartContext } from "../context/cart_context";
+import { useUserContext } from "../context/user_context";
+import { formatPrice } from "../utils/helpers";
+import { useHistory } from "react-router-dom";
+
+const promise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+
+const CheckoutForm = () => {
+  return <h4>hello from Stripe Checkout </h4>;
+};
 
 const StripeCheckout = () => {
-  return <div>StripeCheckout</div>;
+  return (
+    <Wrapper>
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section``;
 
 export default StripeCheckout;
