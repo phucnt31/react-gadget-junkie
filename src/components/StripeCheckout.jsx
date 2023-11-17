8a93a1c4980468a385a75e57907dcbb9dd1ddbab
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-17 10:18:41 +0700

    Render message based success state

--- src/components/StripeCheckout.jsx
+++ src/components/StripeCheckout.jsx
@@ -68,6 +68,19 @@ const CheckoutForm = () => {
 
   return (
     <div>
+      {succeeded ? (
+        <article>
+          <h4>Thank you</h4>
+          <h4>Your payment was successful!</h4>
+          <h4>Redirecting to home page shortly</h4>
+        </article>
+      ) : (
+        <article>
+          <h4>Hello, {myUser?.name}</h4>
+          <p>Your total is {formatPrice(shipping_fee + total_amount)}</p>
+          <p>Test Card Number : 4242 4242 4242 4242</p>
+        </article>
+      )}
       <form id="payment-form" onSubmit={handleSubmit}>
         <CardElement
           id="card-element"
