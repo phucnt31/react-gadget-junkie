3a70d159d46cf17470e34c1587c020f88c7d1ffb
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-17 13:31:57 +0700

    Fix css for add to cart button

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -56,7 +56,7 @@ const AddToCart = ({ product }) => {
         />
         <Link
           to="/cart"
-          className="btn"
+          className="btn add-to-cart"
           onClick={() => addToCart(id, mainColor, amount, product)}
         >
           add to cart
@@ -109,6 +109,9 @@ const Wrapper = styled.section`
   .btn {
     margin-top: 1rem;
     width: 140px;
+  }
+  .add-to-cart {
+    text-align: center;
   }
 `;
 
