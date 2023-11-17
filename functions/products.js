66ef433d9d881646b122bf10de6ced5601cea935
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-17 12:29:45 +0700

    Handle get products from airtable

--- functions/products.js
+++ functions/products.js
@@ -1,6 +1,24 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const Airtable = require("airtable-node");
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base(process.env.AIRTABLE_BASE)
+  .table(process.env.AIRTABLE_TABLE);
+
 exports.handler = async (event, context, cb) => {
-  return {
-    statusCode: 200,
-    body: "single product route",
-  };
+  try {
+    const resp = await airtable.list({ maxRecords: 200 });
+    console.log(resp);
+    return {
+      statusCode: 200,
+      body: "products route",
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: "There was an error",
+    };
+  }
 };
